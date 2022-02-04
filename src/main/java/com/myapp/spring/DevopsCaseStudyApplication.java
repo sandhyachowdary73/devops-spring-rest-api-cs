37c36cd9585b015689067eaa3537ee883b424fb4
sandhyachowdary73 <sandhyachowdary73@gmail.com>
2022-02-04 11:05:48 +0530

    new project

---
+++ src/main/java/com/myapp/spring/DevopsCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCaseStudyApplication.class, args);
+	}
+
+}
