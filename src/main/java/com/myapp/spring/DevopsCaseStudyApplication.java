4f5e18c3f5ec1cf17379c9c9fe2c514e4a24727f
LakshmiDharani <DharaniPaduchuri>
2022-02-04 01:05:35 -0500

    changed by lakshmi

--- src/main/java/com/myapp/spring/DevopsCaseStudyApplication.java
+++ src/main/java/com/myapp/spring/DevopsCaseStudyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// commented by lakshmi
 public class DevopsCaseStudyApplication {
 
 	public static void main(String[] args) {
