96f6e32b03064da9eb1f9e17b5ac076f84106875
andesreenathreddy <andesreenathreddy@gmail.com>
2022-02-22 17:47:31 +0530

    updated

--- src/test/java/com/myapp/test/LoginTest.java
+++ src/test/java/com/myapp/test/LoginTest.java
@@ -1,7 +1,11 @@
 package com.myapp.test;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,17 +22,25 @@ public class LoginTest {
 	WebDriver driver=new ChromeDriver();
 	
 	driver.get("http://127.0.0.1:8899");
-
 	WebElement Username=driver.findElement(By.id("username"));
 	WebElement password=driver.findElement(By.id("password"));
 	WebElement login=driver.findElement(By.name("commit"));
-	Username.sendKeys("username");
-	password.sendKeys("password");
+	Username.sendKeys("Admin1234");
+	password.sendKeys("Test12345");
 	login.click();
-
+	
+	
+	 String expectedTitle = "";
+     String actualTitle = driver.getTitle();
+     assertEquals(actualTitle, expectedTitle);
+	
+		
 	
 	}
 
-	
+	private void assertFalse(String string, boolean equals) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
