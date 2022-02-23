9cf9b46a1060a9f8bcf2cb8d4edac987d1772de6
andesreenathreddy <andesreenathreddy@gmail.com>
2022-02-23 11:28:16 +0530

    updated

--- src/test/java/com/myapp/test/LoginTest.java
+++ src/test/java/com/myapp/test/LoginTest.java
@@ -2,8 +2,10 @@ package com.myapp.test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,25 +16,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginTest {
 	
 	@Test
-	public void login() {
+	public void login() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\drivers\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	
 	driver.get("http://127.0.0.1:8899");
-	WebElement Username=driver.findElement(By.id("username"));
-	WebElement password=driver.findElement(By.id("password"));
-	WebElement login=driver.findElement(By.name("commit"));
-	Username.sendKeys("Admin1234");
+	WebElement Username=driver.findElement(By.name("username"));
+	WebElement password=driver.findElement(By.name("password"));
+	WebElement login=driver.findElement(By.xpath("//button[text()='Log In']"));
+	Username.sendKeys("Admin12345");
 	password.sendKeys("Test12345");
-	login.click();
+	Thread.sleep(500);
+	login.submit();
+	//String actualUrl="http://127.0.0.1:8888";
+//	String expectedUrl= driver.getCurrentUrl();
+	//assertEquals(expectedUrl,actualUrl);
+	
+	 String expectedTitle = "Log in with your account";
+   //  String actualTitle = driver.getTitle();
+   //  assertEquals(actualTitle, expectedTitle);
+	
+	//Validate the actual page title with expected page title using assert equals method
+	//System.out.println("Assert equals method validation");
+	//assertEquals(expectedTitle, driver.getTitle());
 	
 	
-	 String expectedTitle = "";
-     String actualTitle = driver.getTitle();
-     assertEquals(actualTitle, expectedTitle);
+
+	// Page title validation using assert false method
+	System.out.println("Assert false method validation");
+	assertFalse("Title does match", expectedTitle.equals(driver.getTitle()));
 	
-		
+	
+	
+
+	
 	
 	}
 
